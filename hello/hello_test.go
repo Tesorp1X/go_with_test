e6de72e9871b5d34f5d87f5ab65531c099ffb3b8
Tesorp1X <rakteam2014@gmail.com>
2025-03-17 06:25:23 +0700

    Tests and the code are refactired. Redy to go!

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -6,15 +6,18 @@ func TestHello(t *testing.T) {
 	t.Run("say hello to people", func(t *testing.T) {
 		got := Hello("Liz")
 		want := "Hello, Liz"
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrect(t, got, want)
 	})
-	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrect(t, got, want)
 	})
+}
+
+func assertCorrect(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
