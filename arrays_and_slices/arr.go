a72746225820dc168ff2e12bf5c3967ea24bcc59
Tesorp1X <rakteam2014@gmail.com>
2025-03-18 10:04:33 +0700

    SumAllTails now safely handles empty slices

--- arrays_and_slices/arr.go
+++ arrays_and_slices/arr.go
@@ -21,8 +21,13 @@ func SumAllTails(numbresToSum ...[]int) []int {
 	var sums []int
 
 	for _, numbers := range numbresToSum {
-		tail := numbers[1:]
-		sums = append(sums, Sum(tail))
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+
 	}
 
 	return sums
