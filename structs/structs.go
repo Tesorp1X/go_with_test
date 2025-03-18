8902f66cc5e6c2d4c8507fc07839889cda46a4a6
Tesorp1X <rakteam2014@gmail.com>
2025-03-18 19:57:15 +0700

    Rectangle perimetr works

--- structs/structs.go
+++ structs/structs.go
@@ -1,1 +1,10 @@
 package structs
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Perimetr() float64 {
+	return 2 * (r.Height + r.Width)
+}
--- structs/structs_test.go
+++ structs/structs_test.go
@@ -1,1 +1,13 @@
 package structs
+
+import "testing"
+
+func TestPerimetr(t *testing.T) {
+	fig := Rectangle{10.0, 20.0}
+	got := fig.Perimetr()
+	want := 60.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
