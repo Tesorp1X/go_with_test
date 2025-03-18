547c34e164192d6c9c79297b97e025221978c05d
Tesorp1X <rakteam2014@gmail.com>
2025-03-19 01:31:27 +0700

    New test for Rectangles and Circles. Also Refactored

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -3,10 +3,40 @@ package structs
 import "testing"
 
 func TestPerimetr(t *testing.T) {
-	fig := Rectangle{10.0, 20.0}
-	got := fig.Perimetr()
-	want := 60.0
+	t.Run("rectangle", func(t *testing.T) {
+		fig := Rectangle{10.0, 20.0}
+		got := fig.Perimetr()
+		want := 60.0
 
+		assertCorrect(t, got, want)
+	})
+	t.Run("circle", func(t *testing.T) {
+		fig := Circle{10.0}
+		got := fig.Perimetr()
+		want := 2 * 10 * pi
+
+		assertCorrect(t, got, want)
+	})
+}
+
+func TestArea(t *testing.T) {
+	t.Run("rectangle", func(t *testing.T) {
+		fig := Rectangle{10.0, 20.0}
+		got := fig.Area()
+		want := 200.0
+
+		assertCorrect(t, got, want)
+	})
+	t.Run("circle", func(t *testing.T) {
+		fig := Circle{10.0}
+		got := fig.Area()
+		want := 10 * 10 * pi
+
+		assertCorrect(t, got, want)
+	})
+}
+
+func assertCorrect(t testing.TB, got, want float64) {
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
 	}
