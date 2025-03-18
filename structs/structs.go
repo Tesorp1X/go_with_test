090a70590276d0f139234bcc4b46c311e08b8a3d
Tesorp1X <rakteam2014@gmail.com>
2025-03-19 01:32:36 +0700

    New struct Circle and new methods/ Tested and refactored

--- structs/structs.go
+++ structs/structs.go
@@ -1,5 +1,7 @@
 package structs
 
+const pi float64 = 3.1415
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -7,4 +9,20 @@ type Rectangle struct {
 
 func (r Rectangle) Perimetr() float64 {
 	return 2 * (r.Height + r.Width)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Perimetr() float64 {
+	return 2 * pi * c.Radius
+}
+
+func (c Circle) Area() float64 {
+	return pi * c.Radius * c.Radius
 }
