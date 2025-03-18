17ab0236c40e673ac36b4563ecc1c40e95a5fa6e
Tesorp1X <rakteam2014@gmail.com>
2025-03-18 09:35:19 +0700

    Tests for Sum and SumAll are ready and refactored

--- arrays_and_slices/arr_test.go
+++ arrays_and_slices/arr_test.go
@@ -1,6 +1,9 @@
 package arraysandslices
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("Collection of 3 elements", func(t *testing.T) {
@@ -24,4 +27,25 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, expected, numberSlice)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("Sum 2 slices separatly", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{3, 9})
+		expected := []int{3, 12}
+
+		assertCorrect(t, got, expected)
+	})
+	t.Run("Sum 3 slises separatly", func(t *testing.T) {
+		got := SumAll([]int{10, 2}, []int{3, 9, -1}, []int{1, 2, 3, 4})
+		expected := []int{12, 11, 10}
+
+		assertCorrect(t, got, expected)
+	})
+}
+
+func assertCorrect(t testing.TB, got, expected []int) {
+	if !slices.Equal(got, expected) {
+		t.Errorf("got %v want %v", got, expected)
+	}
 }
