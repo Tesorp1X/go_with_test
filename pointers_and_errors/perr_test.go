2636c82d42496a190a0ea24e7684e21fbba73fd3
Tesorp1X <rakteam2014@gmail.com>
2025-03-20 04:49:54 +0700

    Refactoring and new tests for Withdraw

--- pointers_and_errors/perr_test.go
+++ pointers_and_errors/perr_test.go
@@ -5,29 +5,28 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	t.Run("one deposit", func(t *testing.T) {
-		wallet := Wallet{}
-
-		wallet.Deposit(Bitcoin(10))
-
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
 		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %d want %d", got, want)
-		}
-	})
-	t.Run("two deposits", func(t *testing.T) {
-		wallet := Wallet{}
-
-		wallet.Deposit(Bitcoin(10))
-		wallet.Deposit(Bitcoin(10))
-
-		got := wallet.Balance()
-		want := Bitcoin(20)
-
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
 		}
+	}
+	t.Run("one deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+	t.Run("two deposits", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(20))
+
+	})
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 }
