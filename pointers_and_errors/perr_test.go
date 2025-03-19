9d8e6fed694c16c8705c9ac6be6fcdcc61fc7aa5
Tesorp1X <rakteam2014@gmail.com>
2025-03-20 04:37:19 +0700

    New test case for deposit

--- pointers_and_errors/perr_test.go
+++ pointers_and_errors/perr_test.go
@@ -5,14 +5,29 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
+	t.Run("one deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-	wallet.Deposit(10)
+		wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
-	want := 10
+		got := wallet.Balance()
+		want := Bitcoin(10)
 
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+	t.Run("two deposits", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(Bitcoin(10))
+		wallet.Deposit(Bitcoin(10))
+
+		got := wallet.Balance()
+		want := Bitcoin(20)
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
 }
