6a8c33c18df8c93b1aaa8601c13c041ae98065ec
Tesorp1X <rakteam2014@gmail.com>
2025-03-20 05:01:34 +0700

    New test case for Withdraw and refactoring: assertError

--- pointers_and_errors/perr_test.go
+++ pointers_and_errors/perr_test.go
@@ -12,21 +12,38 @@ func TestWallet(t *testing.T) {
 			t.Errorf("got %s want %s", got, want)
 		}
 	}
+	assertError := func(t testing.TB, err error) {
+		t.Helper()
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
+	}
+
 	t.Run("one deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
+
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 	t.Run("two deposits", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
 		wallet.Deposit(Bitcoin(10))
-		assertBalance(t, wallet, Bitcoin(20))
 
+		assertBalance(t, wallet, Bitcoin(20))
 	})
-	t.Run("withdraw", func(t *testing.T) {
+	t.Run("withdraw: no overdraft", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		wallet.Withdraw(Bitcoin(10))
+
 		assertBalance(t, wallet, Bitcoin(10))
+	})
+	t.Run("withdraw: overdraft scenario", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, startingBalance)
+		assertError(t, err)
 	})
 }
