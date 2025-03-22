8d4c40dc1dd1d909e73b59dc2dfb3f336ecc8734
Tesorp1X <rakteam2014@gmail.com>
2025-03-23 05:38:41 +0700

    Done, I guess

--- dependency_injection/depIj_test.go
+++ dependency_injection/depIj_test.go
@@ -1,1 +1,18 @@
 package dependencyinjection
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Liz")
+
+	got := buffer.String()
+	want := "Hello, Liz"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
