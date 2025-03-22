ce7bf3f3ac804caeb270c84100890e5674f362e8
Tesorp1X <rakteam2014@gmail.com>
2025-03-22 07:37:36 +0700

    Search works as ecpectrd

--- maps/maps.go
+++ maps/maps.go
@@ -1,1 +1,12 @@
 package maps
+
+import (
+	"fmt"
+)
+
+func Search(dictionary map[string]string, word string) (string, error) {
+	if val, ok := dictionary[word]; ok {
+		return val, nil
+	}
+	return "", fmt.Errorf("error: couldn't find word %s", word)
+}
