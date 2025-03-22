daf249548c20e7794f91db400dcedb1f40af2a57
Tesorp1X <rakteam2014@gmail.com>
2025-03-22 08:31:51 +0700

    Refactoring: new test-cases for Add and new helper functions

--- maps/maps_test.go
+++ maps/maps_test.go
@@ -9,22 +9,37 @@ func assertStrings(t testing.TB, got, want string) {
 	}
 }
 
-func assertError(t testing.TB, errGot, errWant error) {
+func assertError(t testing.TB, wantErr bool, errGot, errWant error) {
 	t.Helper()
-	if errGot == nil {
+	if errGot != nil && !wantErr {
+		t.Errorf("got unexpected error: %v", errGot)
+		return
+	}
+	if errGot == nil && !wantErr {
+		return
+	}
+	if errGot == nil && wantErr {
 		t.Fatal("expected an error, but got non")
 	}
-	if errGot.Error() != errWant.Error() {
+	if errGot.Error() != errWant.Error() && wantErr {
 		t.Errorf("wantErr: %q, gotErr: %q", errWant, errGot)
 	}
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+	got, err := dictionary.Search(word)
+	assertError(t, false, err, nil)
+	assertStrings(t, got, definition)
 }
 
 func TestSearch(t *testing.T) {
 	t.Run("find existing word", func(t *testing.T) {
 		dictionary := Dictionary{"test": "this is just a test"}
 
-		got, _ := dictionary.Search("test")
+		got, err := dictionary.Search("test")
 		want := "this is just a test"
+		assertError(t, false, err, nil)
 		assertStrings(t, got, want)
 
 	})
@@ -33,6 +48,29 @@ func TestSearch(t *testing.T) {
 
 		_, err := dictionary.Search("tes")
 
-		assertError(t, err, ErrNoSuchWord)
+		assertError(t, true, err, ErrNoSuchWord)
+	})
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("adding new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+
+		dictionary.Add("test", "this is just a test")
+		want := "this is just a test"
+		got, err := dictionary.Search("test")
+
+		assertError(t, false, err, nil)
+		assertStrings(t, got, want)
+	})
+	t.Run("adding existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+
+		_, err := dictionary.Add(word, "new line")
+
+		assertError(t, true, err, ErrWordAlreadyExists)
+		assertDefinition(t, dictionary, word, definition)
 	})
 }
