e185b90f3b21675354d5bea86b3d1d10b3897c4d
Tesorp1X <rakteam2014@gmail.com>
2025-03-23 17:00:01 +0700

    Countdown with sleeper works as it shouls

--- mocking/mocking_test.go
+++ mocking/mocking_test.go
@@ -1,1 +1,34 @@
 package mocking
+
+import (
+	"bytes"
+	"testing"
+)
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func TestCountdown(t *testing.T) {
+	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
+
+	Countdown(buffer, spySleeper)
+
+	got := buffer.String()
+	want := `3
+2
+1
+Go!`
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls of Sleep(): expected 3, but got %q", spySleeper.Calls)
+	}
+}
