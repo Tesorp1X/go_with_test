c4d04a97866939ccdba7d9b0e6b36b3524441766
Tesorp1X <rakteam2014@gmail.com>
2025-03-24 00:50:42 +0700

    Configurable sleeper and refactoring

--- mocking/moching.go
+++ mocking/moching.go
@@ -3,6 +3,7 @@ package mocking
 import (
 	"fmt"
 	"io"
+	"time"
 )
 
 const (
@@ -12,6 +13,15 @@ const (
 
 type Sleeper interface {
 	Sleep()
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
 }
 
 func Countdown(out io.Writer, sleeper Sleeper) {
