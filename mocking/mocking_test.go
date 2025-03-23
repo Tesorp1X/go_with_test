bf4049befc590120d72e607c5bb491559a97d1f4
Tesorp1X <rakteam2014@gmail.com>
2025-03-23 23:40:19 +0700

    Old test is back, to evalute prints itself

--- mocking/mocking_test.go
+++ mocking/mocking_test.go
@@ -1,6 +1,7 @@
 package mocking
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
 )
@@ -32,20 +33,36 @@ func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
 }
 
 func TestCountdown(t *testing.T) {
-	spySleepPrinter := &SpyCountdownOperations{}
-	Countdown(spySleepPrinter, spySleepPrinter)
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		Countdown(buffer, &SpyCountdownOperations{})
 
-	want := []string{
-		write,
-		sleep,
-		write,
-		sleep,
-		write,
-		sleep,
-		write,
-	}
+		got := buffer.String()
+		want := `3
+2
+1
+Go!`
 
-	if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
-		t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
 }
