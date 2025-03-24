7a99b6d87bdd8b1d37a7cfc85c3285a149ab2fb7
Tesorp1X <rakteam2014@gmail.com>
2025-03-24 10:23:20 +0700

    Racer passes the test

--- select/select.go
+++ select/select.go
@@ -1,1 +1,24 @@
 package pselect
+
+import (
+	"net/http"
+	"time"
+)
+
+func Racer(a, b string) (winner string) {
+	statrA := time.Now()
+	http.Get(a)
+	aDuration := time.Since(statrA)
+
+	statB := time.Now()
+	http.Get(b)
+	bDuration := time.Since(statB)
+
+	if aDuration < bDuration {
+		winner = a
+	} else {
+		winner = b
+	}
+
+	return
+}
