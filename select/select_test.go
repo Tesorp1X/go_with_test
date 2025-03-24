bfdb1176b43621d85690e84e0c7a1192bdbfdfbf
Tesorp1X <rakteam2014@gmail.com>
2025-03-24 11:16:36 +0700

    Reimplemented timeout test-caese via ConfigurableRacer

--- select/select_test.go
+++ select/select_test.go
@@ -19,23 +19,24 @@ func TestRacer(t *testing.T) {
 		fastURL := fastServer.URL
 
 		want := fastURL
-		got, _ := Racer(slowURL, fastURL)
+		got, err := Racer(slowURL, fastURL)
+
+		if err != nil {
+			t.Fatalf("didn't expect an error, but got one: %v", err)
+		}
 
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
 		}
 	})
 	t.Run("returns an error if niether of servers respond in 10s", func(t *testing.T) {
-		slowServer := makeDelayedServer(12 * time.Second)
-		fastServer := makeDelayedServer(11 * time.Second)
+		slowServer := makeDelayedServer(30 * time.Millisecond)
 
 		defer slowServer.Close()
-		defer fastServer.Close()
 
-		slowURL := slowServer.URL
-		fastURL := fastServer.URL
+		serverURL := slowServer.URL
 
-		_, err := Racer(slowURL, fastURL)
+		_, err := ConfigurableRacer(serverURL, serverURL, 26*time.Millisecond)
 
 		if err == nil {
 			t.Errorf("expected an error, but got noting")
