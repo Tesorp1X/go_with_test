91d6e20b44488fefb87d93224e6a6709f4d59593
Tesorp1X <rakteam2014@gmail.com>
2025-03-29 04:26:57 +0700

    Walk can now itterate through all struct's fields

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -1,1 +1,11 @@
 package reflection
+
+import "reflect"
+
+func walk(x any, fn func(string)) {
+	val := reflect.ValueOf(x)
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fn(field.String())
+	}
+}
