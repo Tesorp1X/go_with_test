373d8220bec80b904a87d15f5c75a6b5e9f3fe64
Tesorp1X <rakteam2014@gmail.com>
2025-04-08 01:36:12 +0700

    Test for non-concurrent counter

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,1 +1,18 @@
 package sync
+
+import "testing"
+
+func TestCounter(t *testing.T) {
+	counter := Counter{}
+	counter.Inc()
+	counter.Inc()
+	counter.Inc()
+	counter.Inc()
+
+	got := counter.Value()
+	want := 4
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
